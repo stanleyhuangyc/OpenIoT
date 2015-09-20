52cd149848f374b46cc08518317a3ae8aaf8b150
Stanley Huang <stanleyhuangyc@gmail.com>
2015-09-20 20:45:24 +0800

    Update SensorSender.ino

--- sketches/RF24Sensors/SensorSender/SensorSender.ino
+++ sketches/RF24Sensors/SensorSender/SensorSender.ino
@@ -1,42 +1,79 @@
 
 /*
-* Getting Started example sketch for nRF24L01+ radios
-* This is a very basic example of how to send data from one node to another
-* Updated: Dec 2014 by TMRh20
+* This sketch senses temperature/humidity with a DHT11 module
+* and senses current/voltage level with a MAX471 module
+* and transmit data via a nRF24L01 module on SPI
+*
+* Written by Stanley Huang for Project OpenIoT
 */
 
 #include <SPI.h>
-#include "RF24.h"
+#include <RF24.h>
+#include <dht.h>
 
-#define DEV_ID 3
+#define DEV_ID 4
+#define DHT11_PIN 8
+#define CURRENT_PIN A0
+#define VOLTAGE_PIN A1
 
-/* Hardware configuration: Set up nRF24L01 radio on SPI bus plus pins 7 & 8 */
-RF24 radio(7,8);
-/**********************************************************/
+/* Set up nRF24L01 radio on SPI bus plus pins 9 & 10 */
+RF24 radio(9,10);
 
 byte addresses[][6] = {"SNDER","RCVER"};
 
+dht DHT;
+
+#define WORKING_VOLTAGE 4.95
+
 typedef struct {
   byte id;
-  unsigned long time;  
+  unsigned long time;
+  int temperature;
+  int humidity;
+  unsigned int voltage;
+  int current;
 } DATA_BLOCK;
+
+void getVoltAmp(float& v, float& a)
+{
+  int vn = 0;
+  int an = 0;
+
+  for (byte n = 0; n < 10; n++) {
+    vn += analogRead(CURRENT_PIN);
+    an += analogRead(VOLTAGE_PIN);
+    delay(100);
+  }
+  v = (float)vn / 10 / 1024 * WORKING_VOLTAGE * 5;
+  a = (float)an / 10 / 1024 * WORKING_VOLTAGE;
+}
 
 void setup() {
   radio.begin();
 
   // Set the PA Level low to prevent power supply related issues since this is a
- // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
   radio.setPALevel(RF24_PA_MAX);
   
   // Open a writing and reading pipe on each radio, with opposite addresses
-    radio.openWritingPipe(addresses[0]);
-    radio.openReadingPipe(1,addresses[1]);
-  
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
 }
 
 void loop() {
   DATA_BLOCK data = {DEV_ID, millis()};
-  radio.write( &data, sizeof(data));
-  delay(1000);
-} // Loop
 
+  float v;
+  float a;
+  getVoltAmp(v, a);
+  data.voltage = (unsigned int)(v * 100);
+  data.current = (int)(a * 100);  
+  if (DHT.read11(DHT11_PIN) == DHTLIB_OK) {
+    data.temperature = (int)(DHT.temperature * 10);
+    data.humidity = (int)(DHT.humidity * 10);
+  }
+
+  radio.write( &data, sizeof(data));
+
+  delay(2000);
+}
