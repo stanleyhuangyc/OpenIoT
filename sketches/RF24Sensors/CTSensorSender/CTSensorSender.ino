fe482649e6860954ed153fc99da314e08afd6467
Stanley Huang <stanleyhuangyc@gmail.com>
2015-09-28 12:35:27 +1000

    Add CT sensor data sender

---
+++ sketches/RF24Sensors/CTSensorSender/CTSensorSender.ino
@@ -0,0 +1,84 @@
+
+/*
+* This sketch senses temperature/humidity with a DHT11 module
+* and senses AC current with SCT-013 CT sensor
+* and transmit data via a nRF24L01 module on SPI
+*
+* Written by Stanley Huang for Project OpenIoT
+*/
+
+#include <SPI.h>
+#include <RF24.h>
+#include <dht11.h>
+
+#define DEV_ID 1
+#define PIN_DHT11 8
+#define PIN_CURRENT_SENSOR A0
+#define PIN_VOLTAGE_SENSOR A1
+#define CURRENT_SENSOR_RATIO 0.067
+
+/* Set up nRF24L01 radio on SPI bus plus pins 9 & 10 */
+RF24 radio(9,10);
+
+byte addresses[][6] = {"SNDER","RCVER"};
+
+typedef struct {
+  byte id;
+  unsigned long time;
+  int temperature; /* 0.1C */
+  unsigned int humidity; /* 0.1% */
+  unsigned int voltage; /* 1/100V */
+  int current; /* 1/100A */
+  int watt; /* 1/10W */
+} DATA_BLOCK;
+
+dht11 DHT;
+DATA_BLOCK data = {DEV_ID};
+
+void sensePower(float& amp, float& volt, float& watt, unsigned int duration)
+{
+  uint32_t a2n = 0;
+  uint32_t an = 0;
+  uint32_t t = millis();
+  uint32_t n = 0;
+  do {
+    unsigned int a = analogRead(PIN_CURRENT_SENSOR);
+    a2n += (uint32_t)a * a;
+    an += a;
+    n++;
+  } while (millis() - t < duration);
+  // only half cycle valid and half samples counted
+  n /= 2;
+  amp = sqrt((float)a2n / n) * CURRENT_SENSOR_RATIO;
+  watt = amp < 0.05 ? 0 : (float)an * volt / n * CURRENT_SENSOR_RATIO;
+}
+
+void setup() {
+  analogReference(INTERNAL);
+
+  radio.begin();
+
+  // Set the PA Level low to prevent power supply related issues since this is a
+  // getting_started sketch, and the likelihood of close proximity of the devices. RF24_PA_MAX is default.
+  radio.setPALevel(RF24_PA_HIGH);
+  
+  // Open a writing and reading pipe on each radio, with opposite addresses
+  radio.openWritingPipe(addresses[0]);
+  radio.openReadingPipe(1,addresses[1]);
+}
+
+void loop() {
+  float amp = 0, volt = 240, watt = 0;
+  sensePower(amp, volt, watt, 1000);
+  data.time = millis();
+  data.current = amp * 100;
+  data.voltage = volt * 100;
+  data.watt = watt * 10;
+ 
+  if (DHT.read(PIN_DHT11) == DHTLIB_OK) {
+    data.temperature = (int)(DHT.temperature * 10);
+    data.humidity = (int)(DHT.humidity * 10);
+  }
+
+  radio.write( &data, sizeof(data));
+}
